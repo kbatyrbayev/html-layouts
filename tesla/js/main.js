2354197fccf8159c38c38d286ac486afe393ad98
Kairat Batyrbayev <k.batyrbayev>
2022-11-28 14:15:18 +0600

    fix tesla js

--- tesla/js/main.js
+++ tesla/js/main.js
@@ -1,36 +1,39 @@
 $(document).ready(function () {
-  $('#pagepiling').pagepiling({
-    anchors: ['home', 'about', 'gallery', 'contacts'],
-    menu: '#menu',
-    scrollingSpeed: 100,
+  if($(window).width() > 1200) {
+    $('#pagepiling').pagepiling({
+      anchors: ['home', 'about', 'gallery', 'contacts'],
+      menu: '#menu',
+      scrollingSpeed: 100,
+      navigation: {
+        'textColor': '#fff',
+        'bulletsColor': '#fff',
+        'position': 'right',
+        'tooltips': ['Home', 'Feature', 'Gallery', 'Contacts']
+      }
+    });
+  }
+
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 400,
+    slidesPerView: 1,
+  
+    pagination: {
+      el: '.project-paginatation',
+      type: 'bullets',
+      bulletClass: 'project-bullet',
+      bulletActiveClass: 'project-bullet-active',
+      clickable: true
+    },
+  
     navigation: {
-      'textColor': '#fff',
-      'bulletsColor': '#fff',
-      'position': 'right',
-      'tooltips': ['Home', 'Feature', 'Gallery', 'Contacts']
+      nextEl: '.slide-button-next',
+      prevEl: '.slide-button-prev'
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
     }
   });
+  
+  
 });
-console.log(new Swiper);
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  speed: 400,
-  slidesPerView: 1,
-
-  pagination: {
-    el: '.project-paginatation',
-    type: 'bullets',
-    bulletClass: 'project-bullet',
-    bulletActiveClass: 'project-bullet-active',
-    clickable: true
-  },
-
-  navigation: {
-    nextEl: '.slide-button-next',
-    prevEl: '.slide-button-prev'
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  }
-});
-
